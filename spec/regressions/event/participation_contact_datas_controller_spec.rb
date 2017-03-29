d2cad494148ff14e7f0e84667d8106d02ee7fbf8
Pascal Simon <simon@puzzle.ch>
2017-03-29 10:23:08 +0200

    update specs

--- spec/regressions/event/participation_contact_datas_controller_spec.rb
+++ spec/regressions/event/participation_contact_datas_controller_spec.rb
@@ -11,38 +11,76 @@ require 'spec_helper'
 
 describe Event::ParticipationContactDatasController, type: :controller do
 
+  render_views
+
   let(:group) { groups(:top_layer) }
+  let(:course) { Fabricate(:course, groups: [group]) }
+  let(:person) { people(:top_leader) }
+  let(:dom) { Capybara::Node::Simple.new(response.body) }
 
-  let(:event) do
-    course = Fabricate(:course, groups: [group])
-    #course.dates << Fabricate(:event_date, event: course)
-    course
-  end
-
-  let(:user) { people(:top_leader) }
-
-  before { sign_in(user) }
+  before { sign_in(person) }
 
   describe 'GET edit' do
 
     before do
-      event.update!({ hidden_contact_attrs: ['address', 'nickname'] })
+      course.update!({ hidden_contact_attrs: ['address', 'nickname'] })
     end
 
-    let(:dom) { Capybara::Node::Simple.new(response.body) }
-
     it 'does not show hidden contact fields' do
-      get :edit, group_id: event.groups.first.id, event_id: event.id
+      get :edit, group_id: course.groups.first.id, event_id: course.id,
+            event_role: { type: 'Event::Course::Role::Participant' }
 
-      require 'pry'; binding.pry unless $pstop
       expect(dom).to have_selector('input#event_participation_contact_data_first_name')
       expect(dom).to have_selector('input#event_participation_contact_data_last_name')
       expect(dom).to have_selector('input#event_participation_contact_data_email')
 
-      expect(dom).to have_no_selector('input#event_participation_contact_data_title')
+      expect(dom).to have_no_selector('input#event_participation_contact_data_address')
       expect(dom).to have_no_selector('input#event_participation_contact_data_nickname')
     end
 
+  end
+
+  context 'POST update' do
+
+    before do
+      course.update!({ required_contact_attrs: ['nickname', 'address']})
+    end
+
+    it 'validates contact attributes and person attributes' do
+
+      contact_data_params = { first_name: 'Hans', last_name: 'Gugger', email: 'invalid', nickname: '' }
+
+      post :update, group_id: group.id, event_id: course.id,
+             event_participation_contact_data: contact_data_params,
+             event_role: { type: 'Event::Course::Role::Participant' }
+
+      is_expected.to render_template(:edit)
+
+      expect(dom).to have_selector('.alert-error li', text: 'Übername muss ausgefüllt werden')
+      expect(dom).to have_selector('.alert-error li', text: 'Adresse muss ausgefüllt werden')
+      expect(dom).to have_selector('.alert-error li', text: /Email/)
+
+    end
+
+    it 'updates person attributes and redirects to event questions' do
+
+      contact_data_params = { first_name: 'Hans', last_name: 'Gugger',
+                              email: 'dude@example.com', nickname: 'Jojo',
+                              address: 'Street 33' }
+
+      post :update, group_id: group.id, event_id: course.id,
+             event_participation_contact_data: contact_data_params,
+             event_role: { type: 'Event::Course::Role::Participant' }
+
+      is_expected.to redirect_to new_group_event_participation_path(group,
+                                                                    course,
+                                                                    event_role: { type: 'Event::Course::Role::Participant' })
+
+      person.reload
+      expect(person.nickname).to eq('Jojo')
+      expect(person.email).to eq('dude@example.com')
+
+    end
   end
 
 end
